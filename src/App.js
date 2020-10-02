7a4730fb269683ae64c9c25ce87f3d0d5ea722a2
james reantillo <james.reantillo@gmail.com>
2020-10-02 13:26:27 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Movies from './components/movies';
+import './App.css';
+
+function App() {
+  return (
+    <main className='container'>
+      <h1>
+        <Movies />
+      </h1>
+    </main>
+  );
+}
+
+export default App;
