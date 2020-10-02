679228b5fb2bc4b848e4f7ebc9c878f58ade1e54
james reantillo <james.reantillo@gmail.com>
2020-10-02 13:48:47 -0700

    install lodash and added pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -18,6 +20,11 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
+
+  handlePageChange = (page) => {
+    console.log(page);
+  };
+
   render() {
     const { length: count } = this.state.movies;
     if (count === 0) {
@@ -62,6 +69,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
